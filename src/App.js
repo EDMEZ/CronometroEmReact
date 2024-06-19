a4d337baa1461bbdc30ec8b8304ef61706660e9f
Eduardo <eduardomax27@outlook.com>
2024-06-19 16:58:30 -0300

    Projeto Cronometro em REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  const[time, setTime] = useState(0); 
+  const[running,setrunning] = useState(false)
+  const[Paused ,setPaused] = useState(false)
+  
+  
+  const Start = () =>{
+    setrunning(true);
+    setPaused(false);
+  }
+  const Pause =  ()=>{
+    setrunning(false);
+    setPaused(true);
+  }
+  useEffect(()=>{
+    let Interval;
+    
+    if(running && !Paused) {
+      Interval = setInterval(()=>{
+        setTime((time)=> time + 10)
+      },10)
+    }else{
+      clearInterval(Interval)
+    };
+    return () => {
+      clearInterval(Interval);
+    }
+  },[running,Paused]);
+const Reset = () =>{
+  Pause();
+  setTime(0);
+}
+const AddZero = (time) => ("0" + time).slice(-2);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Cron-App'>
+        <h1>Cron</h1>
+        <div className='digits'>
+          <span id='Minutos'>
+            {AddZero(Math.floor((time / 60000) % 60))}
+          </span>
+          <span id='Seconds'>
+            {AddZero(Math.floor((time / 1000) % 60))}  
+          </span>
+          <span id='Ms'>
+            {AddZero((time / 10 ) % 100)}
+          </span>
+        </div>
+        <div className='Actions'>
+          <button disabled={running} className='Btn Btn-Start' onClick={Start}>
+            Iniciar
+          </button>
+          <button className='Btn Btn-Pause' onClick={Pause}>
+            Pausar
+          </button>
+          <button className='Btn Btn-Reset' onClick={Reset}>
+            Resetar
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
